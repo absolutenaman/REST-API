020ffee1f7764fdf28d9d598c2f4ce150c4b7474
absolutenaman <absolute.naman@gmail.com>
2025-08-29 23:31:35 +0530

    Added middlewares for jwt token

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -12,10 +12,11 @@ func Authenticate(context *gin.Context) {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "User not authorized to create an event"})
 		return
 	}
-	err := utils.ValidateToken(token)
+	userId, err := utils.ValidateToken(token)
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "User not authorized to create an event"})
 		return
 	}
+	context.Set("userId", userId)
 	context.Next()
 }
