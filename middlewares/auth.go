1575b33106d488333b27fce197efeba8b65b42ad
absolutenaman <absolute.naman@gmail.com>
2025-10-19 19:18:49 +0530

    Fixed the auth middleware bugsuing context.abort in which not authorised users were also able to create events

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -10,11 +10,13 @@ func Authenticate(context *gin.Context) {
 	token := context.Request.Header.Get("Authorization")
 	if token == "" {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "User not authorized to create an event"})
+		context.Abort()
 		return
 	}
 	userId, err := utils.ValidateToken(token)
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "User not authorized to create an event"})
+		context.Abort()
 		return
 	}
 	context.Set("userId", userId)
